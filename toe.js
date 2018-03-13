a42a9d267253ae35c763e8c5f58487be0657cd19
Itzik Fekete <myfekete@gmail.com>
2018-03-13 10:19:36 +0000

    added a bit of JS

--- toe.js
+++ toe.js
@@ -1,4 +1,33 @@
-function xando (){
-  $("#xButton").draggable();
-  $("#oButton").draggable();
-}
+var xButton = document.querySelector('.xButton');
+var oButton = document.querySelector('.oButton');
+console.log(xButton);
+
+var a1= document.querySelector('#a1');
+var a2= document.querySelector('#a2');
+var a3= document.querySelector('#a3');
+var b1= document.querySelector('#b1');
+var b2= document.querySelector('#b2');
+var b3= document.querySelector('#b3');
+var c1= document.querySelector('#c1');
+var c2= document.querySelector('#c2');
+var c3= document.querySelector('#c3');
+
+// class grid {
+// constructor (a1, a2, a3, b1, b2, b3, c1, c2, c3){
+// this._a1=document.querySelector('#a1');
+// this._a2=a2;
+// this._a3=a3;
+// this._b1=b1;  
+// this._b2=b2;
+// this._b3=b3;
+// this._c1=c1;
+// this._c2=c2;
+// this._c3=c3;
+// console.log('this._a1');
+// }
+// }
+
+// function xando (){
+//   $("#xButton").draggable();
+//   $("#oButton").draggable();
+// }
